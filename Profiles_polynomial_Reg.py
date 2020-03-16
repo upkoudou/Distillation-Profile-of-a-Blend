4f74b2c7dddb36a0f5452bbccb9e4de37ec88a33
Pacome KOUDOU <31732881+upkoudou@users.noreply.github.com>
2020-03-15 21:25:39 -0400

    Update Profiles_polynomial_Reg.py

--- Profiles_polynomial_Reg.py
+++ Profiles_polynomial_Reg.py
@@ -9,11 +9,9 @@ dataset = pd.read_csv('Crude-File.csv')
 X = dataset.iloc[:, 0:-1].values
 y = dataset.iloc[:, 1:].values
 
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection  import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.linear_model import LinearRegression
@@ -24,15 +22,14 @@ poly_reg.fit(X_poly, y)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
-
 # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
+plt.title('Crude oil - Temperature/Volume')
+plt.xlabel('Volume')
+plt.ylabel('Temperature')
 plt.show()
 
 #Model score
