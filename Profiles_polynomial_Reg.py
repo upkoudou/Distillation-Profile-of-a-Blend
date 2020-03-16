47ea533a67777fe20206b9de2fd5b55db0973f84
Pacome KOUDOU <31732881+upkoudou@users.noreply.github.com>
2020-03-15 21:20:44 -0400

    Update Profiles_polynomial_Reg.py

--- Profiles_polynomial_Reg.py
+++ Profiles_polynomial_Reg.py
@@ -1,198 +1,55 @@
 
-# coding: utf-8
-
-# In[16]:
-
-
-# impor necessary packages  
-from numpy import *
-from matplotlib.pyplot import *
-from pandas import *
-from pandas import DataFrame
-import csv
-from matplotlib import style
-style.use("ggplot")
-from scipy.interpolate import *
-from sklearn import datasets
+# Importing the libraries
 import numpy as np
-from numpy.polynomial.polynomial import polyfit
 import matplotlib.pyplot as plt
-
-from sklearn import linear_model
-from sklearn.preprocessing import PolynomialFeatures 
-
-
-# In[3]:
-
-
-pwd()
-
-
-# In[4]:
-
-
 import pandas as pd
-data = pd.read_csv('crude-file.csv')
-data.head(10)
+
+# Importing the dataset
+dataset = pd.read_csv('Crude-File.csv')
+X = dataset.iloc[:, 0:-1].values
+y = dataset.iloc[:, 1:].values
 
 
-# In[5]:
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection  import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-data.describe()
-
-
-# In[6]:
-
-
-#plot the distribution of the data 
-data.plot(kind='hist', alpha=.4, legend=True) # alpha for transparency
-plt.xlabel('Temperature C')
-plt.ylabel('Volume evaporated %')
-plt.title('Distribution') 
-
-
-# In[7]:
-
-
-#plot my data
-y_profile = data.Volume
-x_profile = data.temperature
-
-plt.scatter(x_profile, y_profile)
-plt.xlabel('Temperature C')
-plt.ylabel('Volume evaporated %')
-plt.title('Volume evaporation by temperature') 
-plt.show
-
-
-# In[8]:
-
-
-#simple linear reg model, see fit
-b, m = polyfit(x_profile, y_profile, 1)
-fit = m*x_profile + b
-plt.plot(x_profile, y_profile, 'bo')
-plt.plot(x_profile, fit,'r-')
-plt.xlabel('Temperature C')
-plt.ylabel('Volume evaporated %')
-plt.title('Linear Regression fit') 
-
-
-# In[9]:
-
-
-#results & Coefs trick
-from sklearn import datasets, linear_model
+# Fitting Polynomial Regression to the dataset
 from sklearn.linear_model import LinearRegression
-import statsmodels.api as sm
-from scipy import stats
-
-constant = y_profile
-y = sm.add_constant(constant)
-est = sm.OLS(x_profile, y)
-est2 = est.fit()
-print(est2.summary())
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 5)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
 
 
-# In[10]:
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
 
+#Model score
+from sklearn.metrics import mean_squared_error, r2_score
 
-X = data.temperature.values.reshape(len(data.temperature.values), 1)
-y = data.Volume.values.reshape(len(data.temperature.values), 1)
+#train
+y_poly_pred_train_ = lin_reg_2.predict(poly_reg.fit_transform(X_train))
 
+train_rmse = np.sqrt(mean_squared_error(y_train,y_poly_pred_train_))
+train_r2 = r2_score(y_train,y_poly_pred_train_)
+print(train_rmse)
+print(train_r2)
 
-# In[11]:
+#test
+y_poly_pred_test_ = lin_reg_2.predict(poly_reg.fit_transform(X_test))
 
-
-#redo the linear regression with a built-in function to stay in line with further steps (poly)
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
-
-# Visualizing the Linear Regression results
-def viz_linear():
-    plt.scatter(X, y, color='blue')
-    plt.plot(X, lin_reg.predict(X), color='red')
-    plt.xlabel('Temperature C')
-    plt.ylabel('Volume evaporated %')
-    plt.title('Linear Regression fit') 
-    plt.show()
-    return
-viz_linear()
-
-
-# In[13]:
-
-
-#build fundtion to return coef of goodness R**2 = 1-(SSYhat/Mean of Ys)
-
-def squared_error(ys_orig,ys_line):
-    return sum((ys_line - ys_orig) * (ys_line - ys_orig))
-
-def coefficient_of_determination(ys_orig,ys_line):
-    y_mean_line = [mean(ys_orig) for y in ys_orig]
-    squared_error_regr = squared_error(ys_orig, ys_line)
-    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
-    return 1 - (squared_error_regr/squared_error_y_mean)
-
-
-# In[14]:
-
-
-#Linear model coef of goodness/Determination
-
-r_squared = coefficient_of_determination(X, y)
-print(r_squared)
-
-
-# In[18]:
-
-
-# Even though the R**2 valu is more satisfying at a glance we can easily deduct that the simple linear model
-#isn't the best fit, let's try with a polynomial model to the nTh degree until best fit. 
-
-# Visualising the Polynomial Regression results 
-
-poly = PolynomialFeatures(degree = 5) 
-X_poly = poly.fit_transform(X)  
-
-poly.fit(X_poly, y) 
-lin2 = LinearRegression() 
-lin2.fit(X_poly, y) 
-
-plt.scatter(X, y, color = 'blue') 
-  
-plt.plot(X, lin2.predict(poly.fit_transform(X)), color = 'red') 
-plt.xlabel('Temperature C')
-plt.ylabel('Volume evaporated %')
-plt.title('Polynomial Regression Fit') 
-  
-plt.show() 
-
-
-# In[19]:
-
-
-#Polynomial model coef of goodness/Determination at order 5
-from sklearn.metrics import r2_score
-y_true = y_profile 
-y_pred = y
-
-
-# In[20]:
-
-
-r2_score(y_true, y_pred) ##1 ?? a perfect model is questionable
-
-
-# In[ ]:
-
-
-/*the(Polynomial, model, fit, our, data, better,, the, R**2, value, has, also, increased,, the, next, step, would, have, been, to, look)
-for the limitations of the model, by splitting my dataset two parts, testing and trainnig. 
-Train the model and test the model on the data to appreciate its accuracy.
-
-The conditions concerning the residuals are assumed to be validated. 
-*/
-
+test_rmse = np.sqrt(mean_squared_error(y_test,y_poly_pred_test_))
+test_r2 = r2_score(y_test,y_poly_pred_test_)
+print(test_rmse)
+print(test_r2)
